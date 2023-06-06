9cf02bf9b4a0bba0d172a5b6cf945ead3f095666
HaraldFranck <harald@franck.nu>
2023-06-06 14:35:43 +0200

    gjort det pænere

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ def search_movies():
     director = request.form.get('director')
     yearfrom = request.form.get('yearfrom')
     yearto = request.form.get('yearto')
+    keywords = request.form.get('keywords')
+    rating = request.form.get('rating')
 
     # Set up a database connection
     conn =  get_db_connection()
@@ -31,7 +33,7 @@ def search_movies():
 
     try:
         # Construct the SQL query based on the search criteria
-        query = "SELECT movies.title, movies.year, movies.genres, movies.director FROM movies"
+        query = "SELECT DISTINCT movies.title, movies.year, movies.genres, movies.director, movies.overview, movies.rating FROM movies"
 
         # Join acts_in table to fetch movies where the specified actor is involved
         if actor:
@@ -43,12 +45,15 @@ def search_movies():
         if title:
             conditions.append("movies.title ILIKE %s")
             values.append(f"%{title}%")
+
         if actor:
             conditions.append("acts_in.actor ILIKE %s")
             values.append(f"%{actor}%")
+
         if genre:
             conditions.append("movies.genres ILIKE %s")
             values.append(f"%{genre}%")
+
         if director:
             conditions.append("movies.director ILIKE %s")
             values.append(f"%{director}%")
@@ -61,13 +66,23 @@ def search_movies():
             conditions.append("movies.year <= %s")
             values.append(yearto)
 
+        if keywords:
+            conditions.append("movies.overview ILIKE %s")
+            values.append(f"%{keywords}%")
+        
+        if rating:
+            conditions.append("movies.rating >= %s")
+            values.append(rating)
+
         if conditions:
             query += " WHERE " + " AND ".join(conditions)
 
         # Execute the query
         cursor.execute(query, values)
         movies = cursor.fetchall()
-        return render_template('search_results.html', movies=movies)
+        length = []
+        length.append(len(movies))
+        return render_template('search_results.html', movies=movies, length=length)
 
     finally:
         # Close the cursor and connection
