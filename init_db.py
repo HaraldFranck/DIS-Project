e9515039ccb1e77645f8c59f2df87b6aed6e9bef
HaraldFranck <harald@franck.nu>
2024-05-23 14:34:21 +0200

    create database

--- init_db.py
+++ init_db.py
@@ -1,15 +1,34 @@
 import psycopg2
-import csv 
+import csv
 
-conn = psycopg2.connect(
-        host="localhost",
-        database="movies",
-        user='postgres',
-        password='passwor')
+# Database connection parameters
+db_params = {
+    'host': "localhost",
+    'user': 'postgres',
+    'password': 'passwor'
+}
 
-# Open a cursor to perform database operations
+# Connect to the default 'postgres' database to check for the existence of the 'movies' database
+conn = psycopg2.connect(**db_params, database='postgres')
+conn.autocommit = True  # Enable autocommit mode for database creation
 cur = conn.cursor()
 
+# Check if the 'movies' database exists
+cur.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = 'movies'")
+exists = cur.fetchone()
+
+# If the database does not exist, create it
+if not exists:
+    cur.execute('CREATE DATABASE movies')
+
+cur.close()
+conn.close()
+
+# Now connect to the newly created or existing 'movies' database
+conn = psycopg2.connect(**db_params, database='movies')
+cur = conn.cursor()
+
+# Drop and create tables
 cur.execute('DROP TABLE IF EXISTS movies CASCADE;')
 cur.execute('CREATE TABLE movies (title varchar (150),'
                                  'year integer NOT NULL,'
@@ -29,8 +48,9 @@ cur.execute('CREATE TABLE acts_in (actor varchar (40),'
                                 'year integer,'
                                 'FOREIGN KEY (movie, year) REFERENCES movies (title,year));')
 
+# Insert data into tables
 with open('imdb_top_1000.csv', 'r', encoding="UTF-8") as f:
-    reader = csv.reader(f, )
+    reader = csv.reader(f)
     next(reader) # Skip the header row.
     for row in reader:
         runtime = row[4][:2]
@@ -44,7 +64,7 @@ with open('imdb_top_1000.csv', 'r', encoding="UTF-8") as f:
         row[7], metascore, row[9], row[15]))
 
 with open('imdb_top_1000.csv', 'r', encoding="UTF-8") as f:
-    reader = csv.reader(f, )
+    reader = csv.reader(f)
     next(reader) # Skip the header row.
     for row in reader:
         cur.execute(
@@ -59,7 +79,6 @@ with open('imdb_top_1000.csv', 'r', encoding="UTF-8") as f:
         cur.execute(
         "INSERT INTO acts_in VALUES (%s, %s, %s)", 
         (row[13], row[1], row[2]))
-
 
 conn.commit()
 
